9efafe77014b11e86bc9d1c06c6c074d7e7dc949
ctf <ctf@rock-chips.com>
2024-03-07 20:27:50 +0800

    rkmuxer: update static library 1.adaptive file cache 2.open add fake fd mechanism 3.distinguish between normal and aov modes
    rkmuxer change-Id: I827a3f2cca4a12d7b15904765ea56471f3a860f4
    
    Signed-off-by: ctf <ctf@rock-chips.com>
    Change-Id: I23965c6afe0a5fe5f0b10e51e0232ab8398caa2f

--- misc/include/file_common.h
+++ misc/include/file_common.h
@@ -3,6 +3,18 @@
 #ifndef __FILE_COMMON_H__
 #define __FILE_COMMON_H__
 
+#include <pthread.h>
+
+typedef void (*FILE_CACHE_SDCARD_LOCK)(void);
+typedef void (*FILE_CACHE_SDCARD_UNLOCK)(void);
+typedef int (*FILE_CACHE_MOUMNT_SDCARD)(void);
+typedef int (*FILE_CACHE_UMOUMNT_SDCARD)(void);
+
+typedef enum _FILE_WRITE_MODE {
+	NORMAL_MODE = 0,
+	AOV_MODE,
+} FILE_WRITE_MODE;
+
 typedef enum _FILE_SCHED_POLICY {
 	FILE_SCHED_OTHER = 0,
 	FILE_SCHED_BATCH,
@@ -16,11 +28,19 @@ typedef struct _FILE_WRITE_THREAD_ARG {
 	int priority;                 /* SCHED_OTHER/SCHED_IDLE/SCHED_BATCH inoperative */
 } FILE_WRITE_THREAD_ARG;
 
+typedef struct _FILE_SDCARD_ARG {
+	FILE_CACHE_SDCARD_LOCK lock;
+	FILE_CACHE_SDCARD_UNLOCK unlock;
+	FILE_CACHE_MOUMNT_SDCARD mount_sdcard;
+	FILE_CACHE_UMOUMNT_SDCARD umount_sdcard;
+} FILE_SDCARD_ARG;
+
 typedef struct _FILE_CACHE_ARG {
 	const char *sdcard_path;
 	int write_cache;
 	int total_cache;
 	FILE_WRITE_THREAD_ARG write_thread_arg;
+	FILE_SDCARD_ARG sdcard_arg;
 } FILE_CACHE_ARG;
 
 #endif
