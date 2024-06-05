0ca3f4daa5ca4e03513c5155d9c440c10eeffa13
Xing Zheng <zhengxing@rock-chips.com>
2024-06-05 17:28:34 +0800

    audio: demo: clean up and update demo for SED
    Signed-off-by: Xing Zheng <zhengxing@rock-chips.com>
    Change-Id: If9934e1bc5640fc51c013335fdcf53e502394d13

--- audio/rkaudio_algorithms/demo/readme.txt
+++ audio/rkaudio_algorithms/demo/readme.txt
@@ -1,12 +1,17 @@
-执行命令：
-./test_sed 输入音频文件地址  输出音频文件地址
-例如： ./test_sed  ./dbg_capin.wav ./dbg_capin_out.wav
+编译：
+$YOUR_COMPILE_PATH/arm-rockchip830-linux-uclibcgnueabihf-gcc -o test_sed sed_main.c -I include/ -L ../uclibc/ -lrkaudio_detect -lrkaudio_common
 
-带有outfile的lib  在设定环境变量PATH_ORI_IN ， PATH_NET_IN ，PATH_NET_OUT 后
+./test_sed total.wav outsed.wav /oem/usr/share/vqefiles/rkaudio_model_sed_bcd.rknn
+
+执行命令：
+./test_sed 输入音频文件地址  输出音频文件地址  SED模型全路径
+
+例如：
+./test_sed sed_in.wav sed_out.wav /oem/usr/share/vqefiles/rkaudio_model_sed_bcd.rknn
+
+其中outsed.wav，第一通道为语音信噪比snr_db的检测结果，第二通道为超大声lsd_db的检测结果，第三通道为哭声BCD的检测结果。
+
+带有outfile的lib，在设定环境变量PATH_ORI_IN，PATH_NET_IN，PATH_NET_OUT后
 将在对应位置生成ori_in.wav  net_in.wav  net_out.wav用于分析样例
 例如：export PATH_ORI_IN=/USERDATA/ori_in.wav
 
-
-md5sum
-814f25624fd064613b136b35d8db211a  librkaudio_detect.so
-99d6bce7d46fd2c6e2fe298f43ef4b02  librkaudio_detect.a
---
+++ audio/rkaudio_algorithms/demo/sed_main.c
@@ -0,0 +1,158 @@
+#include <assert.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+
+#include "audio/wave_reader.h"
+#include "audio/wave_writer.h"
+#include "skv/rkaudio_sed.h"
+
+#define IN_SIZE 256
+#define ENABLE_NPU 1
+#define ENABLE_DUMP_PCM 0
+
+int main(int argc, char *argv[])
+{
+    int ret = 0;
+    double Total_sample = 0.0;
+    // 读取数据并处理
+    clock_t startTime, endTime;
+    clock_t clk_start, clk_end;
+
+    /* For Debug  */
+    int aed_stat = 0;
+    int out_size = 0, in_size = 0, res = 0;
+    int bcd_stat = 0, bcd_stat_old = 0;
+
+    if (argc < 3)
+    {
+        printf("Error: Wrong input parameters! A example is as following: \n");
+        printf("fosafer_enh test_in.wav test_out.wav 0\n");
+        exit(-1);
+    }
+
+    char *in_filename = argv[1];
+    char *out_filename = argv[2];
+
+#if ENABLE_DUMP_PCM
+    putenv("PATH_ORI_IN=/userdata/ori_in.wav");
+    putenv("PATH_NET_IN=/userdata/net_in.wav");
+    putenv("PATH_NET_OUT=/userdata/net_out.wav");
+    putenv("DIR_NEG_SAMPLE=/userdata/neg_sample/");
+#endif
+
+#if ENABLE_NPU
+    char *sed_rknn_path = argv[3];
+    printf("sed_rknn_path is %s\n", sed_rknn_path);
+    int tmpres = rkaudio_sed_bcd_model_set(sed_rknn_path);
+#endif
+    // for wave reader
+    wave_reader *wr;
+    wave_reader_error rerror;
+
+    // for wave writer
+    wave_writer *ww;
+    wave_writer_error werror;
+    wave_writer_format format;
+
+    // 读取输入音频
+    wr = wave_reader_open(in_filename, &rerror);
+    if (!wr)
+    {
+        printf("rerror=%d\n", rerror);
+        return -1;
+    }
+
+    int mSampleRate = wave_reader_get_sample_rate(wr);
+    int mBitPerSample = wave_reader_get_sample_bits(wr);
+    int mNumChannel = wave_reader_get_num_channels(wr);
+
+    // 输入检查
+    if (mNumChannel > 1)
+    {
+        printf("This algorithm is a single channel algorithm and will run on the first channel of data\n");
+    }
+
+    // 每次读取数据大小
+    int read_size = IN_SIZE * mNumChannel * mBitPerSample / 8;
+    char *in = (char *)calloc(1, read_size * sizeof(char));
+    int out_res_num = 3;
+    short *out = (short *)calloc(1, IN_SIZE * out_res_num * sizeof(short));
+    // 输出音频格式设置
+    format.num_channels = out_res_num;
+    format.sample_rate = mSampleRate;
+    format.sample_bits = mBitPerSample;
+    ww = wave_writer_open(out_filename, &format, &werror);
+    // 输出音频建立失败
+    if (!ww)
+    {
+        printf("werror=%d\n", werror);
+        wave_reader_close(wr);
+        return -1;
+    }
+
+    // 声音事件检测初始化
+    RKAudioSedRes sed_res;
+    RKAudioSedParam *sed_param = rkaudio_sed_param_init();
+    void *st_sed = rkaudio_sed_init(mSampleRate, mBitPerSample, mNumChannel, sed_param);
+    char initres = rkaudio_sed_init_res(st_sed);
+    rkaudio_sed_param_destroy(sed_param);
+    if (st_sed == NULL)
+    {
+        printf("Failed to create baby cry handle\n");
+        return -1;
+    }
+
+loop_again:
+    startTime = clock();
+    int cnt = 0;
+
+    int gbs = 1, buz = 1, bcd = 1;
+    int  bcdcounts = 0;
+    while (0 < (res = wave_reader_get_samples(wr, IN_SIZE, in)))
+    {
+        in_size = res * (mBitPerSample / 8) * mNumChannel;
+        cnt++;
+        clk_start = clock();
+        out_size = rkaudio_sed_process(st_sed, (short *)in, in_size / 2, &sed_res);
+        float lsd_res =  rkaudio_sed_lsd_db(st_sed);
+        if (out_size < 0)
+            fprintf(stderr, "bcd process return error=%d\n", out_size);
+
+        clk_end = clock();
+
+        //printf("lsd=%d,snr=%d,bcd=%d,buz_res=%d,gbs_res=%d\n", sed_res.lsd_res, sed_res.snr_res, sed_res.bcd_res, sed_res.buz_res, sed_res.gbs_res);
+        if (sed_res.bcd_res == 1)
+            bcdcounts++;
+
+        // 输出，测试用
+        for (int i = 0; i < IN_SIZE; i++)
+        {
+            *(out + out_res_num * i)     = 10000 * sed_res.snr_res;
+            *(out + out_res_num * i + 1) = 10000 * sed_res.lsd_res;
+            *(out + out_res_num * i + 2) = 10000 * sed_res.bcd_res;
+        }
+        wave_writer_put_samples(ww, IN_SIZE, out);
+        Total_sample += in_size / 2 / mNumChannel;
+        //if (cnt % 63 == 0)
+        //  printf("cnt = %d\n", cnt);
+    }
+    endTime = clock();
+
+    printf("\nFinished, speech_time = %f, cost_time = %f\n", \
+           Total_sample / mSampleRate, (double)(endTime - startTime) / CLOCKS_PER_SEC);
+
+    wave_writer_close(ww, &werror);
+    wave_reader_close(wr);
+
+    free(in);
+    free(out);
+
+    // 释放
+    if (st_sed)
+        rkaudio_sed_destroy(st_sed);
+
+    printf("Sed test finished\n");
+    return 0;
+}
